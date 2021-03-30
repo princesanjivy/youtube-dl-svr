1be37a16c6f0e54f3369c9b0f1008073f1b17728
PrinceSanjivy <sanjivy.android@gmail.com>
2021-03-30 23:58:48 +0530

    audio url

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def getVideoData(url):
     ydl = ytdl.YoutubeDL(ydl_opts)
     with ydl:
         res = ydl.extract_info(url, download=False)
-    return res["formats"][len(res["formats"])-1]["url"], res["title"], res["formats"]
+    return res["formats"][len(res["formats"])-1]["url"], res["title"], res["formats"][1]["url"]
 
 @app.route("/geturl", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         data = request.json
-        video_url, title, vidData = getVideoData(data["yt_url"])
-        return jsonify({"video_url": video_url, "title": title, "data": vidData})
+        video_url, title, audio_url = getVideoData(data["yt_url"])
+        return jsonify({"video_url": video_url, "title": title, "audio_url": audio_url})
     else:
         return "nothing here!"
 
